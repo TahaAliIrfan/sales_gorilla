6dd0b85ce4cd5165aedcf31f231084012a855fb2
Taha Ali Irfan <tahairfan1993@gmail.com>
2025-03-20 17:50:53 +0500

    Add customer status border class and refactor index view to utilize it
    - Introduced a new helper method `customer_status_border_class` to determine the appropriate border class based on customer status.
    - Refactored the customer index view to use the new helper method for cleaner code and improved maintainability.

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,16 @@
 module CustomersHelper
+  def customer_status_border_class(customer)
+    case customer.status
+    when 'Pending' then 'border-l-4 border-yellow-400'
+    when 'Contact Established' then 'border-l-4 border-green-400'
+    when 'Contact Not Established' then 'border-l-4 border-red-400'
+    when 'Unresponsive' then 'border-l-4 border-orange-400'
+    when 'Converted' then 'border-l-4 border-blue-400'
+    when 'Proposal Sent' then 'border-l-4 border-indigo-400'
+    when 'Not Interested' then 'border-l-4 border-gray-400'
+    when 'Exhausted' then 'border-l-4 border-purple-400'
+    when 'Invalid' then 'border-l-4 border-purple-400'
+    else 'border-l-4 border-gray-200'
+    end
+  end
 end
