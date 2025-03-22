0ba524d026668cfc4807d3bbefec3c3d0614f7a0
Taha Ali Irfan <tahairfan1993@gmail.com>
2025-03-23 01:18:22 +0500

    remove gibrish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,6 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env['omniauth.auth']
-
-    puts "ashgasghghsa"
-    puts "ashgasghghsa"
-    puts "ashgasghghsa"
-    puts "ashgasghghsa"
-    puts "ashgasghghsa"
-    puts auth
-    puts "ashgasghghsa"
-    puts "ashgasghghsa"
-    puts "ashgasghghsa"
-    puts "ashgasghghsa"
-    puts "ashgasghghsa"
     user = User.find_or_create_by(provider: auth.provider, uid: auth.uid) do |u|
       u.name = auth.info.name
       u.email = auth.info.email
