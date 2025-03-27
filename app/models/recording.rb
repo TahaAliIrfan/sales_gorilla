ee262e6afccfeeaf401c7dd47874607647a686b0
Taha Ali Irfan <tahairfan1993@gmail.com>
2025-03-28 03:08:13 +0500

    Add helper methods for JSONB transcription data in Recording model

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -30,4 +30,34 @@ class Recording < ApplicationRecord
   def request_transcription
     TranscribeRecordingsWorker.perform_async(id) unless transcribed?
   end
+
+  # Helper methods for working with JSONB transcription data
+  def full_transcript
+    return nil unless transcribed?
+    transcription.dig('results', 'channels', 0, 'alternatives', 0, 'transcript')
+  end
+
+  def transcript_by_speaker
+    return {} unless transcribed?
+    
+    words = transcription.dig('results', 'channels', 0, 'alternatives', 0, 'words') || []
+    words.group_by { |word| word['speaker'] }.transform_values do |speaker_words|
+      speaker_words.map { |w| w['punctuated_word'] }.join(' ')
+    end
+  end
+
+  def transcript_metadata
+    return nil unless transcribed?
+    transcription['metadata']
+  end
+
+  def transcript_confidence
+    return nil unless transcribed?
+    transcription.dig('results', 'channels', 0, 'alternatives', 0, 'confidence')
+  end
+
+  def transcript_words
+    return [] unless transcribed?
+    transcription.dig('results', 'channels', 0, 'alternatives', 0, 'words') || []
+  end
 end
