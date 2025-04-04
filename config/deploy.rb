92f5ceecc50ac18e22a4bc8c4d227753ff99a743
Taha Ali Irfan <tahairfan1993@gmail.com>
2025-04-04 05:28:38 +0500

    Update deployment branch to master: Changed the branch setting in both deploy.rb and production.rb files from 'wapi_implementation' to 'master' for consistency in deployment configurations.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:tahairfan13/crm.git"
 set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-set :branch,          'wapi_implementation'
+set :branch,          'master'
 set :passenger_restart_with_touch, true
 # Don't change these unless you know what you're doing
 set :pty,             true
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,7 @@
 
 set :stage, :production
 set :rails_env, :production
-set :branch, "wapi_implementation"
+set :branch, "master"
 set :ssh_options, {
   forward_agent: false,
   auth_methods: %w[publickey],
